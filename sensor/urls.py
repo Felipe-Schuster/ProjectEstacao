3605778012aa966a59df18588191e061705c5852
Felipe Schuster <felipeariel93@gmail.com>
2025-02-04 19:53:28 -0400

    atualizar

--- sensor/urls.py
+++ sensor/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('sensors/', views.SensorDataAPIView.sensor_api, name='sensor_api'),
-    path('api/sensors', views.SensorDataAPIView.as_view(), name='sensor-data'),
+    path('api/sensors/', views.SensorDataAPIView.as_view(), name='sensor-data'),
     path('', views.index, name='index'),
 ]
